c1ae130e7a9ef5e469cde9708c01537bee652fb6
Rosana Rezende <rezende_rosana@hotmail.com>
2020-07-21 14:52:51 -0300

    tabela User criada

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://b8077ff7f73140:2a25c8b6@us-cdbr-east-02.cleardb.com/heroku_37fd418817fd19e'
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(120), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
 @app.route('/')
 def index():
-  return 'Hello World!'
+    return 'Hello World!'
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
