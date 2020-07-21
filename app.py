0ba61bed0c10e27b74b153060de4813deb35edac
Rosana Rezende <rezende_rosana@hotmail.com>
2020-07-21 14:21:29 -0300

    conexão com o DB do Heroku

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import *
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://b8077ff7f73140:2a25c8b6@us-cdbr-east-02.cleardb.com/heroku_37fd418817fd19e'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
